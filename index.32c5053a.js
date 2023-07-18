6d43beb2d6593df39add834a749595e95bc7571b
Andrew Dagdag <zerinout@gmail.com>
2023-07-19 00:53:57 +0800

    Updates

---
+++ index.32c5053a.js
@@ -0,0 +1,2 @@
+window.onload=()=>{let e=document.getElementById("container")?.children[0];null!=e&&(e.innerHTML="Some cool stuffs"),console.log("yeehaw I started")};
+//# sourceMappingURL=index.32c5053a.js.map
